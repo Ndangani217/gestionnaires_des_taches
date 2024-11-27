0502f6bb33ec408050cc10db317757ea8916d6ed
Heritier Ndangani <heritierndangani217@gmail.com>
2024-11-27 09:02:05 +0100

    Fin de tâche : création d'un tableau pour stocker les tâches

---
+++ index.js
@@ -0,0 +1,29 @@
+const taches = [
+    {
+        nom:'Lecture de livres',
+        dateEcheance: new Date(2024-11-24),
+        priorite:'Haute',
+        estTerminee:true
+    },
+
+    {
+        nom:'Aller au cinéma',
+        dateEcheance: new Date(2024-11-25),
+        priorite:'Moyenne',
+        estTerminee:false
+    },
+
+    {
+        nom:'S\'exèrcé avec javascript',
+        dateEcheance: new Date(2023-11-26),
+        priorite:'Haute',
+        estTerminee:true
+    },
+
+    {
+        nom:'Joueur au foot',
+        dateEcheance: new Date(2023-11-27),
+        priorite:'Basse',
+        estTerminee:false
+    }
+] 
